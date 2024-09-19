02cc8d0cf9c8b5673a55bae654479dd4da5ec44d
TraxxasDeluxed <144713587+TraxxasDeluxed@users.noreply.github.com>
2024-09-19 16:18:24 +0000

    added base

--- APCSA_mad_lib.java
+++ APCSA_mad_lib.java
@@ -1,1 +1,27 @@
-//hello world
+class APCSA_mad_lib{
+    public static void main(String[] args) {
+        // creates a scanner to process terminal input
+        Scanner state = new Scanner(System.in);
+        
+        // prompts the user so they know when 
+        // to enter keyboard input within a program
+        System.out.println("Choose a story: ");
+        
+        // nextLine method returns all keyboard input
+        // until the user presses the Return or Enter key
+        String userInput = state.nextLine();
+        
+        if(state == 1){
+            
+        }
+
+        if(state == 2){
+
+        }
+
+        if(state == 3){
+
+        }
+        
+    }
+}
