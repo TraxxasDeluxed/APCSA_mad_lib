16f0dde0f0af16b171b18a0c800756ef98933264
TraxxasDeluxed <144713587+TraxxasDeluxed@users.noreply.github.com>
2024-09-26 17:02:49 +0000

    need to change the hardcoded values to suit the requirement.

--- APCSA_mad_lib.java
+++ APCSA_mad_lib.java
@@ -1,27 +1,56 @@
-class APCSA_mad_lib{
+import java.util.Scanner;
+
+class APCSA_mad_lib {
     public static void main(String[] args) {
-        // creates a scanner to process terminal input
+        //mad libs
+        StringBuilder storyBuilder = new StringBuilder();
+        storyBuilder.append("I went to the animal (noun), the (plural_noun) and the (number) beasts were there. ");
+        storyBuilder.append("Once upon a time in a (adjective) land, a (noun) (adverb) grew. ");
+        storyBuilder.append("(name) went to school at (schoolName) in (year)");
+
+
+        // replace, dont use hardcoded methods
         Scanner state = new Scanner(System.in);
-        
-        // prompts the user so they know when 
-        // to enter keyboard input within a program
-        System.out.println("Choose a story: ");
-        
-        // nextLine method returns all keyboard input
-        // until the user presses the Return or Enter key
-        String userInput = state.nextLine();
-        
-        if(state == 1){
-            
-        }
 
-        if(state == 2){
+        System.out.println("Choose a noun");
+        String nounInput = state.nextLine();
 
-        }
+        System.out.println("Choose a plural noun");
+        String pnounInput =  state.nextLine();
 
-        if(state == 3){
+        System.out.println("Choose a number");
+        String numInput =  state.nextLine();
 
-        }
+        System.out.println("Choose an adjective");
+        String adjInput = state.nextLine();
+
+        System.out.println("Choose a name");
+        String nameInput = state.nextLine();
+
+        System.out.println("Choose a school name");
+        String schoolNameInput = state.nextLine();
+
+        System.out.println("Choose a year");
+        String yearInput = state.nextLine();
+
         
+
+        String story = storyBuilder.toString()
+            .replace("(adjective)", adjInput)
+            .replace("(noun)", nounInput)
+            .replace("(adverb)", pnounInput)
+            .replace("(noun2)", numInput)
+            .replace("(adjective)", adjInput)
+            .replace("(name)", nameInput)
+            .replace("(schoolName)", schoolNameInput)
+            .replace("(year)", yearInput);
+
+            System.out.println("Here is your story");
+            System.out.println(story);
+
+        
+
+
+
     }
 }
