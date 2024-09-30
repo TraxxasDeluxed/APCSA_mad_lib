f53d170f8b2bd2923d6f787f96ed8de260baad7a
TraxxasDeluxed <144713587+TraxxasDeluxed@users.noreply.github.com>
2024-09-30 19:10:39 +0000

    created the code for the mad lib, need to fix it

--- APCSA_mad_lib.java
+++ APCSA_mad_lib.java
@@ -2,53 +2,47 @@ import java.util.Scanner;
 
 class APCSA_mad_lib {
     public static void main(String[] args) {
-        //mad libs
-        StringBuilder storyBuilder = new StringBuilder();
-        storyBuilder.append("I went to the animal (noun), the (plural_noun) and the (number) beasts were there. ");
-        storyBuilder.append("Once upon a time in a (adjective) land, a (noun) (adverb) grew. ");
-        storyBuilder.append("(name) went to school at (schoolName) in (year)");
+        
+        Scanner scanner = new Scanner(System.in);
+        String madlib = "done";
 
+        while (true){
 
-        // replace, dont use hardcoded methods
-        Scanner state = new Scanner(System.in);
+            System.out.println("Choose a number between 1 to 3");
+            String ans = scanner.nextLine(); 
 
-        System.out.println("Choose a noun");
-        String nounInput = state.nextLine();
+        if (ans.compareTo("one")==0){
+            madlib = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
+            break;
+        }
 
-        System.out.println("Choose a plural noun");
-        String pnounInput =  state.nextLine();
+        if (ans.compareTo("two") == 0){
+             madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+             break;
+        }
 
-        System.out.println("Choose a number");
-        String numInput =  state.nextLine();
+        if (ans.compareTo("three") == 0){
+             madlib = "<name> went to <school> in <year>";
+             break;
+        }
+        else{
+            System.out.println("Please input the correct number");
+        }
 
-        System.out.println("Choose an adjective");
-        String adjInput = state.nextLine();
+        while(madlib.indexOf("<")!=-1){
+            String angleLocator = madlib.substring(madlib.indexOf("<"), madlib.indexOf(">")+1);
 
-        System.out.println("Choose a name");
-        String nameInput = state.nextLine();
+            System.out.println("Please give me a"+angleLocator.substring(1,angleLocator.length()-1));
+            String word = scanner.nextLine();
+            madlib = madlib.replace(angleLocator,word);
+            System.out.println(madlib);
+        }
 
-        System.out.println("Choose a school name");
-        String schoolNameInput = state.nextLine();
-
-        System.out.println("Choose a year");
-        String yearInput = state.nextLine();
+    }
 
         
 
-        String story = storyBuilder.toString()
-            .replace("(adjective)", adjInput)
-            .replace("(noun)", nounInput)
-            .replace("(adverb)", pnounInput)
-            .replace("(noun2)", numInput)
-            .replace("(adjective)", adjInput)
-            .replace("(name)", nameInput)
-            .replace("(schoolName)", schoolNameInput)
-            .replace("(year)", yearInput);
 
-            System.out.println("Here is your story");
-            System.out.println(story);
-
-        
 
 
 
