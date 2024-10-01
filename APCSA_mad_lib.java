89d718bb1473fa6581a5aca47400e6ddb7e7e3f1
TraxxasDeluxed <144713587+TraxxasDeluxed@users.noreply.github.com>
2024-10-01 15:44:05 +0000

    project completed

--- APCSA_mad_lib.java
+++ APCSA_mad_lib.java
@@ -2,49 +2,43 @@ import java.util.Scanner;
 
 class APCSA_mad_lib {
     public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-        String madlib = "done";
 
-        while (true){
+        Scanner scanner = new Scanner(System.in);
+        String madlib = null;
+
+        while (true) {
 
             System.out.println("Choose a number between 1 to 3");
-            String ans = scanner.nextLine(); 
+            String ans = scanner.nextLine();
 
-        if (ans.compareTo("one")==0){
-            madlib = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
-            break;
+            if (ans.compareTo("one") == 0) {
+                madlib = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
+                break;
+            }
+
+            if (ans.compareTo("two") == 0) {
+                madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+                break;
+            }
+
+            if (ans.compareTo("three") == 0) {
+                madlib = "<name> went to <school> in <year>";
+                break;
+            } else {
+                System.out.println("Please input the correct number");
+            }
+
         }
 
-        if (ans.compareTo("two") == 0){
-             madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
-             break;
-        }
+        while (madlib.indexOf("<") != -1) {
+            String angleLocator = madlib.substring(madlib.indexOf("<"), madlib.indexOf(">") + 1);
+            System.out.println("Please give me a" + angleLocator.substring(1, angleLocator.length() - 1));
 
-        if (ans.compareTo("three") == 0){
-             madlib = "<name> went to <school> in <year>";
-             break;
-        }
-        else{
-            System.out.println("Please input the correct number");
-        }
-
-        while(madlib.indexOf("<")!=-1){
-            String angleLocator = madlib.substring(madlib.indexOf("<"), madlib.indexOf(">")+1);
-
-            System.out.println("Please give me a"+angleLocator.substring(1,angleLocator.length()-1));
             String word = scanner.nextLine();
-            madlib = madlib.replace(angleLocator,word);
-            System.out.println(madlib);
+            madlib = madlib.replace(angleLocator, word);
+
         }
-
-    }
-
-        
-
-
-
-
+        System.out.println(madlib);
 
     }
 }
