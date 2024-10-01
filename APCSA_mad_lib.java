a115ab457615772660761c8f17f9b021189e0549
TraxxasDeluxed <144713587+TraxxasDeluxed@users.noreply.github.com>
2024-10-01 15:48:36 +0000

    added comments

--- APCSA_mad_lib.java
+++ APCSA_mad_lib.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 class APCSA_mad_lib {
     public static void main(String[] args) {
-
+        // initialize Scanner class
         Scanner scanner = new Scanner(System.in);
         String madlib = null;
-
+        // while loop to choose which madlib you want
         while (true) {
 
-            System.out.println("Choose a number between 1 to 3");
+            System.out.println("Choose a number between one to three");
             String ans = scanner.nextLine();
 
             if (ans.compareTo("one") == 0) {
@@ -29,15 +29,17 @@ class APCSA_mad_lib {
             }
 
         }
-
+        // to get the location of the angle brackets and then replace the anglelocator
+        // form the word typed
         while (madlib.indexOf("<") != -1) {
             String angleLocator = madlib.substring(madlib.indexOf("<"), madlib.indexOf(">") + 1);
-            System.out.println("Please give me a" + angleLocator.substring(1, angleLocator.length() - 1));
+            System.out.println("Please give me a " + angleLocator.substring(1, angleLocator.length() - 1));
 
             String word = scanner.nextLine();
             madlib = madlib.replace(angleLocator, word);
 
         }
+        // prints the output
         System.out.println(madlib);
 
     }
